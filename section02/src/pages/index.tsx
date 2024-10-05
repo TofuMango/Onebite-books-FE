f942df85663e6b0ba80eca8037e67412208c38ed
TofuMango <yjlee0006@cu.ac.kr>
2024-10-05 16:37:21 +0900

    20241005 2.6 스타일 적용

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,3 +1,14 @@
+//CSS Module
+// index.module.css 파일안에 들어있는 모든 클래스 네임들이 자동으로 유니크한 값으로 변환
+import style from './index.module.css'
+
 export default function Home() {
-  return <h1>인덱스</h1>
+  return (
+    <>
+      {/* return <h1 style={{ color: "red" }}>인덱스</h1>; */}
+      {/* 인라인 스타일을 지정해줄수는 있으나, 가독성을 해침 */}
+      <h1 className={style.h1}>인덱스</h1>
+      <h2 className={style.h2}>H2</h2>
+    </>
+  );
 }
